896962294900f5020a657ff2902b5cb70cd348b0
gl-Chen <zdcs0705@163.com>
2022-09-02 20:52:54 +0800

    备份一

---
+++ slot/src/components/Myslot.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <div class="mydiv">
+      <h1 class="title">{{ title }}</h1>
+      <span v-for="(item, index) in categroy" :key="index">
+        {{ item }}
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["categroy", "title"],
+};
+</script>
+
+<style>
+.mydiv {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 15px;
+  margin-left: 15px;
+  width: 300px;
+  height: 500px;
+  background-color: burlywood;
+}
+.title {
+  text-align: center;
+  width: 100%;
+  background-color: yellow;
+}
+</style>
